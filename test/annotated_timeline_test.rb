57e1b3f241d61dbceeb996d29ee25223f987d053
Jeff Latz <jeff8448@yahoo.com>
2008-08-28 12:12:08 -0400

    now tests for value insertion and proper sorting of data

--- test/annotated_timeline_test.rb
+++ test/annotated_timeline_test.rb
@@ -15,24 +15,19 @@ include AnnotatedTimeline
 require File.dirname(__FILE__) + '/test_helper.rb'
 
 class AnnotatedTimelineTest < Test::Unit::TestCase
-  # Replace this with your real tests.
-  
-  #'Sun Aug 24 17:06:15 -0400 2008' 'Sun Aug 26 17:06:15 -0400 2008' 'Sun Aug 28 17:06:15 -0400 2008'
-  
+
   def test_this_plugin
-    # @timestamp1= OpenStruct.new(:year => '2008')
-    # @timestamp2= OpenStruct.new(:year => '2008')
-    # @timestamp3= OpenStruct.new(:year => '2008')
-    
-    output = annotated_timeline({Time.now =>{:foo=>7, :bar=>9}, 1.days.ago=>{:foo=>6, :bar=>10}, 2.days.ago=>{:foo=>5, :bar=>4}})
+    output  = annotated_timeline({Time.now =>{:foo=>7, :bar=>9}, 1.days.ago=>{:foo=>6, :bar=>10}, 2.days.ago=>{:foo=>5, :bar=>4}, 3.days.ago=>{:foo=>6, :bar=>2}, 4.days.ago=>{:foo=>9, :bar=>7}})
+    output2 = annotated_timeline({3.days.ago=>{:foo=>6, :bar=>2}, Time.now =>{:bar=>9, :foo=>7}, 2.days.ago=>{:bar=>4, :foo=>5}, 1.days.ago=>{:foo=>6, :bar=>10}, 4.days.ago=>{:bar=>7, :foo=>9}})
     assert_match(/data\.addColumn\('date', 'Date'\);/, output, "Should Make Date Column")
     assert_match(/data\.addColumn\('number', 'Bar'\);/, output, "Should Make Bar Column")
     assert_match(/data\.addColumn\('number', 'Foo'\);/, output, "Should Make Foo Column")
+    # assert_match(/data\.setValue\(0, 0, new Date\(4.days.ago.year, \(4.days.ago.month-1\), 4.days.ago.date\)\);/, output, "should put the first date in properly")
+    assert_match(/data\.setValue\(0, 1, 7\);/, output, "should put right value for bar" )
+    assert_match(/data\.setValue\(0, 2, 9\);/, output, "should put right value for foo")
+    assert_match(output, output2, "data should be sorted properly")
   end
-    # 
-    # def time_ext_stub
-    #    
-    # end
+
   
   
 end
